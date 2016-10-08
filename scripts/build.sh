7a409407dc4ee2cecf65f9b7147857aba0b85047
Aleks <aleksiev.nikolay@gmail.com>
2016-10-07 23:06:11 -0700

    Trying to fix Travis CI

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,10 +8,10 @@ build_mac()
     local ver=$1
     mkdir .build/macos_$ver
 
-    clang++ -c -std=c++11 -x objective-c++ -mmacosx-version-min=$ver mtlpp.mm -o .build/macos_$ver/mtlpp.o
+    clang++ -c -std=c++11 -x objective-c++ -mmacosx-version-min=$ver mtlpp.mm -o .build/macos_$ver/mtlpp.o -DMTLPP_CONFIG_USE_AVAILABILITY=0
 
-    clang++ -std=c++11 -mmacosx-version-min=$ver examples/00_init.cpp .build/macos_$ver/mtlpp.o -o .build/macos_$ver/00_init -framework Metal -framework CoreFoundation -fobjc-link-runtime
-    clang++ -std=c++11 -mmacosx-version-min=$ver examples/01_clear.cpp .build/macos_$ver/mtlpp.o -o .build/macos_$ver/01_clear -framework Metal -framework CoreFoundation -fobjc-link-runtime
+    clang++ -std=c++11 -mmacosx-version-min=$ver examples/00_init.cpp .build/macos_$ver/mtlpp.o -o .build/macos_$ver/00_init -framework Metal -framework CoreFoundation -fobjc-link-runtime -DMTLPP_CONFIG_USE_AVAILABILITY=0
+    clang++ -std=c++11 -mmacosx-version-min=$ver examples/01_clear.cpp .build/macos_$ver/mtlpp.o -o .build/macos_$ver/01_clear -framework Metal -framework CoreFoundation -fobjc-link-runtime -DMTLPP_CONFIG_USE_AVAILABILITY=0
 
     .build/macos_$ver/00_init
     .build/macos_$ver/01_clear
