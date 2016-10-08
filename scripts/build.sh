f2f27d9c2bba12cb49e1ad4213d57fde56335d99
Aleks <aleksiev.nikolay@gmail.com>
2016-10-07 22:50:16 -0700

    Added build script

---
+++ scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+
+rm -Rf .build
+mkdir .build
+
+build_mac()
+{
+    local ver=$1
+    mkdir .build/macos_$ver
+
+    clang++ -c -std=c++11 -x objective-c++ -mmacosx-version-min=$ver ../mtlpp.mm -o .build/macos_$ver/mtlpp.o
+
+    clang++ -std=c++11 -mmacosx-version-min=$ver ../examples/00_init.cpp .build/macos_$ver/mtlpp.o -o .build/macos_$ver/00_init -framework Metal -framework CoreFoundation -fobjc-link-runtime
+    clang++ -std=c++11 -mmacosx-version-min=$ver ../examples/01_clear.cpp .build/macos_$ver/mtlpp.o -o .build/macos_$ver/01_clear -framework Metal -framework CoreFoundation -fobjc-link-runtime
+
+    .build/macos_$ver/00_init
+    .build/macos_$ver/01_clear
+}
+
+build_mac 10.9
+build_mac 10.10
+build_mac 10.11
+build_mac 10.12
