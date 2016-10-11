b6e407768ff50ed34c7bdd382c735ac875b662aa
Aleks <aleksiev.nikolay@gmail.com>
2016-10-10 23:06:22 -0700

    Added tvOS build to travis-ci.

--- scripts/build_osx.sh
+++ scripts/build_osx.sh
@@ -29,33 +29,57 @@ build_ios()
     local objcflags="-std=c++11 -x objective-c++ -miphoneos-version-min=$ver $sdk -arch $arch"
     local cflags="-std=c++11 -miphoneos-version-min=$ver $sdk -arch $arch"
     local ldflags="-framework Metal -framework CoreFoundation -fobjc-link-runtime"
-    local cc="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++"
 
     rm -Rf $output
     mkdir -p $output
 
-    $cc $objcflags -c ../mtlpp.mm -o $output/mtlpp.o
+    clang++ $objcflags -c ../mtlpp.mm -o $output/mtlpp.o
 
-    $cc $cflags $ldflags ../examples/00_init.cpp $output/mtlpp.o -o $output/00_init
-    $cc $cflags $ldflags ../examples/01_clear.cpp $output/mtlpp.o -o $output/01_clear
-    $cc $cflags $ldflags ../examples/02_triangle.cpp $output/mtlpp.o -o $output/02_triangle
+    clang++ $cflags $ldflags ../examples/00_init.cpp $output/mtlpp.o -o $output/00_init
+    clang++ $cflags $ldflags ../examples/01_clear.cpp $output/mtlpp.o -o $output/01_clear
+    clang++ $cflags $ldflags ../examples/02_triangle.cpp $output/mtlpp.o -o $output/02_triangle
+}
+
+build_tvos()
+{
+    local ver=$1
+    local arch="$2"
+    local output="../.build/tvos_${ver}_${arch}"
+    local sdk="--sysroot=/Applications/Xcode.app/Contents/Developer/Platforms/AppleTVOS.platform/Developer/SDKs/AppleTVOS.sdk"
+    local objcflags="-std=c++11 -x objective-c++ -mtvos-version-min=$ver $sdk -arch $arch"
+    local cflags="-std=c++11 -mtvos-version-min=$ver $sdk -arch $arch"
+    local ldflags="-framework Metal -framework CoreFoundation -fobjc-link-runtime"
+
+    rm -Rf $output
+    mkdir -p $output
+
+    clang++ $objcflags -c ../mtlpp.mm -o $output/mtlpp.o
+
+    clang++ $cflags $ldflags ../examples/00_init.cpp $output/mtlpp.o -o $output/00_init
+    clang++ $cflags $ldflags ../examples/01_clear.cpp $output/mtlpp.o -o $output/01_clear
+    clang++ $cflags $ldflags ../examples/02_triangle.cpp $output/mtlpp.o -o $output/02_triangle
 }
 
 if [[ "$arch" == "arm" ]]; then
-    build_ios 7.0 armv7
-    build_ios 8.0 armv7
-    build_ios 9.0 armv7
-    build_ios 10.0 armv7
+    if [[ "$platform" == "ios" ]]; then
+        build_ios 7.0 armv7
+        build_ios 8.0 armv7
+        build_ios 9.0 armv7
+        build_ios 10.0 armv7
 
-    build_ios 7.0 armv7s
-    build_ios 8.0 armv7s
-    build_ios 9.0 armv7s
-    build_ios 10.0 armv7s
+        build_ios 7.0 armv7s
+        build_ios 8.0 armv7s
+        build_ios 9.0 armv7s
+        build_ios 10.0 armv7s
 
-    build_ios 7.0 arm64
-    build_ios 8.0 arm64
-    build_ios 9.0 arm64
-    build_ios 10.0 arm64
+        build_ios 7.0 arm64
+        build_ios 8.0 arm64
+        build_ios 9.0 arm64
+        build_ios 10.0 arm64
+    else
+        build_tvos 9.0 arm64
+        build_tvos 10.0 arm64
+    fi
 else
     build_mac 10.9
     build_mac 10.10
