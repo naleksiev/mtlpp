df3590117a6eec240cdb539b40af4c9692f9e7a1
Aleks <aleksiev.nikolay@gmail.com>
2017-01-28 00:06:52 -0800

    Added triangle to 04_window.

--- examples/04_window.cpp
+++ examples/04_window.cpp
@@ -1,8 +1,10 @@
 #include "../mtlpp.hpp"
 #include "window.hpp"
 
-mtlpp::Device       g_device;
-mtlpp::CommandQueue g_commandQueue;
+mtlpp::Device              g_device;
+mtlpp::CommandQueue        g_commandQueue;
+mtlpp::Buffer              g_vertexBuffer;
+mtlpp::RenderPipelineState g_renderPipelineState;
 
 void Render(const Window& win)
 {
@@ -10,13 +12,14 @@ void Render(const Window& win)
     uint32_t height = win.GetHeight();
 
     mtlpp::CommandBuffer commandBuffer = g_commandQueue.CommandBuffer();
-    assert(commandBuffer);
 
     mtlpp::RenderPassDescriptor renderPassDesc = win.GetRenderPassDescriptor();
     if (renderPassDesc)
     {
         mtlpp::RenderCommandEncoder renderCommandEncoder = commandBuffer.RenderCommandEncoder(renderPassDesc);
-        assert(renderCommandEncoder);
+        renderCommandEncoder.SetRenderPipelineState(g_renderPipelineState);
+        renderCommandEncoder.SetVertexBuffer(g_vertexBuffer, 0, 0);
+        renderCommandEncoder.Draw(mtlpp::PrimitiveType::Triangle, 0, 3);
         renderCommandEncoder.EndEncoding();
         commandBuffer.Present(win.GetDrawable());
     }
@@ -27,8 +30,43 @@ void Render(const Window& win)
 
 int main()
 {
+    const char shadersSrc[] = R"""(
+        #include <metal_stdlib>
+        using namespace metal;
+
+        vertex float4 vertFunc (
+            const device packed_float3* vertexArray [[buffer(0)]],
+            unsigned int vID[[vertex_id]])
+        {
+            return float4(vertexArray[vID], 1.0);
+        }
+
+        fragment half4 fragFunc ()
+        {
+            return half4(1.0);
+        }
+    )""";
+
+    const float vertexData[] =
+    {
+         0.0f,  1.0f, 0.0f,
+        -1.0f, -1.0f, 0.0f,
+         1.0f, -1.0f, 0.0f,
+    };
     g_device = mtlpp::Device::CreateSystemDefaultDevice();
     g_commandQueue = g_device.NewCommandQueue();
+
+    mtlpp::Library library = g_device.NewLibrary(shadersSrc, mtlpp::CompileOptions(), nullptr);
+    mtlpp::Function vertFunc = library.NewFunction("vertFunc");
+    mtlpp::Function fragFunc = library.NewFunction("fragFunc");
+
+    g_vertexBuffer = g_device.NewBuffer(vertexData, sizeof(vertexData), mtlpp::ResourceOptions::CpuCacheModeDefaultCache);
+
+    mtlpp::RenderPipelineDescriptor renderPipelineDesc;
+    renderPipelineDesc.SetVertexFunction(vertFunc);
+    renderPipelineDesc.SetFragmentFunction(fragFunc);
+    renderPipelineDesc.GetColorAttachments()[0].SetPixelFormat(mtlpp::PixelFormat::RGBA8Unorm);
+    g_renderPipelineState = g_device.NewRenderPipelineState(renderPipelineDesc, nullptr);
 
     Window win(g_device, &Render, 320, 240);
     Window::Run();
